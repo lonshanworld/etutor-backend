48879588f93a9bfd031d07533eaaeb8580fdffde
htetaungko_panasiatower_net <htet.aung.ko@panasiatower.net>
2025-03-31 15:53:25 +0630

    added log message

--- app/Http/Controllers/Api/Files/DeleteFileController.php
+++ app/Http/Controllers/Api/Files/DeleteFileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Files;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Files\DeleteFileRequest;
 use App\Models\File;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class DeleteFileController extends Controller
@@ -20,6 +21,9 @@ class DeleteFileController extends Controller
                 'message' => 'File deleted successfully'
             ]);
         } catch (\Throwable $th) {
+            Log::info('delete file api', [
+                'message' => $th->getMessage(),
+            ]);
             return response()->json([
                 'message' => $th->getMessage()
             ], 500);

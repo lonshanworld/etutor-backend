329f4f4024c440eeb1ef54676bb4b15aa52c37b5
htetaungko_panasiatower_net <htet.aung.ko@panasiatower.net>
2025-04-04 09:26:23 +0630

    modified comment api

--- app/Http/Controllers/Api/Blog/Comments/CommentToBlogController.php
+++ app/Http/Controllers/Api/Blog/Comments/CommentToBlogController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api\Blog\Comments;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Comment\StoreCommentRequest;
+use App\Http\Resources\Api\Blogs\CommentResource;
 use App\Models\Blog;
+use App\Models\Comment;
 use Illuminate\Support\Facades\Log;
 
 class CommentToBlogController extends Controller
@@ -15,12 +17,26 @@ class CommentToBlogController extends Controller
             $validated = $storeCommentRequest->validated();
             $user = auth('sanctum')->user();
             $post = Blog::where('id', $validated['blog_id'])->firstOrFail();
-            $post->comments()->create([
-                'content' => $validated['content'],
-                'user_id' => $user->id
-            ]);
 
-            return response()->success();
+            // $post->comments()->create([
+            //     'content' => $validated['content'],
+            //     'user_id' => $user->id
+            // ]);
+
+            if(! $post) {
+                return response()->error('Blog not found', 404);
+            }
+
+           $comment = Comment::create([
+                'content' => $validated['content'],
+                'user_id' => $user->id,
+                'blog_id' => $post->id
+            ]);
+            
+
+            return response()->success([
+                'comment' => new CommentResource($comment->load('user'))
+            ]);
         } catch (\Throwable $th) {
             Log::error('comment post api', [
                 'message' => $th->getMessage()

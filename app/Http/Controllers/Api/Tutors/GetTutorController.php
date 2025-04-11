38f4bc81a841d315b5514d91ecd289c09d9568f6
yamimie97 <mimie.yhw18@gmail.com>
2025-04-11 21:08:55 +0700

    name search restrict

--- app/Http/Controllers/Api/Tutors/GetTutorController.php
+++ app/Http/Controllers/Api/Tutors/GetTutorController.php
@@ -22,9 +22,9 @@ class GetTutorController extends Controller
                 ->when($request->search, function ($query) use ($request) {
                     $query->where(function ($q) use ($request) {
                         $q->where('email', $request->search)
-                            ->orWhere('first_name', 'like', '%' . $request->search . '%')
-                            ->orWhere('middle_name', 'like', '%' . $request->search . '%')
-                            ->orWhere('last_name', 'like', '%' . $request->search . '%');
+                            ->orWhere('first_name', 'like', $request->search . '%')
+                            ->orWhere('middle_name', 'like', $request->search . '%')
+                            ->orWhere('last_name', 'like', $request->search . '%');
                     });
                 })
                 ->when($request->filter, function ($query) use ($request) {

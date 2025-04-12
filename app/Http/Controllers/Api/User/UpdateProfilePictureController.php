75e5b3d457a89d0573ba90d27ed366faadc0f733
htetaungko <htetaungko@yahoo.com>
2025-04-12 22:25:47 +0630

    added profile picture upload api

---
+++ app/Http/Controllers/Api/User/UpdateProfilePictureController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\User\UpdateProfilePictureRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+
+class UpdateProfilePictureController extends Controller
+{
+    public function __invoke(UpdateProfilePictureRequest $updateProfilePictureRequest)
+    {
+        try {
+            $validated = $updateProfilePictureRequest->validated();
+            $profilePicture = $validated['profile_picture'];
+
+            $originalFilename = $profilePicture->getClientOriginalName();
+            $path = $profilePicture->storeAs('etuto/attachments', $originalFilename, 's3');
+            Storage::disk('s3')->url($path);
+
+            return response()->json([
+                'message' => 'updated'
+            ], 200);
+        } catch (\Throwable $th) {
+            //throw $th;
+            Log::info('profile picture update api', [
+                'message' => $th->getMessage()
+            ]);
+            return response()->error();
+        }
+    }
+}
